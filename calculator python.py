2a5c84f1d1df884701ba4abddb4efb0441bf798c
sowmyathirunavukkarasu-cmd <sowmyathirunavukkarasu@gmail.com>
2026-02-21 12:55:02 +0530

    Add files via upload

---
+++ calculator python.py
@@ -0,0 +1,15 @@
+number1 =input('enter a number 1')
+num1 = int(number1)
+sign = input ('enter the sign')
+number2 =input ('enter a number 2')
+num2 = int(number2)
+if sign == '+':
+     print('your number' + str (num1+num2))
+elif sign == '-':
+    print('your number' + str (num1-num2))
+elif sign == '/':
+    print('your number' + str (num1/num2))
+elif sign == '*':
+    print('your number' + str (num1*num2))
+    
+          
